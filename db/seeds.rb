0ae63dbff7a006347313ef119e1640962a6b132d
Daniel Arenas <daniel1144151@gmail.com>
2026-05-01 23:40:15 -0500

    configurate mongo DB

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Game.destroy_all
+
+Game.create!(
+  name: "The Impostor",
+  description: "A social deduction game. The fisherman must find the impostor before they blend in with the knowers."
+)
+
+Game.create!(
+  name: "Quick Draw",
+  description: "A fast-paced drawing game where speed is everything. (Coming soon)"
+)
