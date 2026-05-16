64191f8cf1dd7d26fd195737bf5fe1aa2ee844ca
Daniel Arenas <daniel1144151@gmail.com>
2026-05-15 22:20:51 -0500

    create question model

--- app/models/how_want_be_billionare/question.rb
+++ app/models/how_want_be_billionare/question.rb
@@ -1,10 +1,11 @@
-module Fisherman
+module HowWantBeBillionare
   class Question
     include Mongoid::Document
     include Mongoid::Timestamps
 
     field :text, type: String
     field :answerds, type: Array
+    field :topic, type: String
 
   end
 end
--- app/services/game_services/how_want_be_billionare.rb
+++ app/services/game_services/how_want_be_billionare.rb
@@ -2,9 +2,12 @@
 module GameServices
   class HowWantBeBillionare < Base
     def initialize(room)
+      @room = room
+      @players = room.players.to_a
     end
 
     def setup_game!
+
       
     end
   end
